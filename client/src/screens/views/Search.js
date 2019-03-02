cec6e4b6ad3150703448bab44bf4461b4069b79b
Norbert Takács <bokker11@hotmail.com>
2019-03-02 23:53:21 +0100

    Added search ui and view.

--- client/src/screens/views/Search.js
+++ client/src/screens/views/Search.js
@@ -1,16 +1,64 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {View, FlatList, StyleSheet} from "react-native";
+import {SearchBar} from "react-native-elements";
+import SearchItem from "../../components/SearchItem/SearchItem";
+import {connect} from "react-redux";
 
 class Search extends Component {
     constructor(props){
         super(props);
     }
 
+    onSearchInputTextChange = (input) => {
+        //Take a copy of the state
+    };
+
     render() {
         return (
-            <View/>
+            <View style={styles.container}>
+                {console.log(this.props.recipes)}
+                <FlatList
+                    data={this.props.recipes}
+                    renderItem={({item}) => (
+                        <SearchItem recipe={item}/>
+                    )}
+                    keyExtractor={recipeObject => recipeObject.id.toString()}
+                    ListHeaderComponent={
+                        <SearchBar
+                            onChangeText={this.onSearchInputTextChange}
+                            placeholder="Search"
+                            lightTheme
+                            round
+                        />
+                    }
+                />
+
+            </View>
+
         )
     }
 }
 
-export default Search;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        alignSelf: "center",
+        width: "100%",
+    }
+});
+
+const mapStateToProps = state => {
+    return {
+        recipes: state.values.recipes,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+//
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
